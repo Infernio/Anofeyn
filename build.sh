7d0aa71e53c011770da36b36332457b6fa030576
Infernio <infernio@icloud.com>
2018-12-31 14:58:40 +0100

    Based the build script on my shared build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Builds a full release of Anofeyn, compiling scripts, packing resources into a
+# Builds a full release of a mod, compiling scripts, packing resources into a
 # BSA and bundling the whole thing in a 7z archive.
 
 # Color constants
@@ -10,7 +10,9 @@ C_BLUE='\033[1;34m'
 C_YELLOW='\033[1;33m'
 C_NC='\033[0m'
 
-# Variable definitions
+# Variable definitions relating to your Skyrim installation
+# These will probably have to be changed to fit your paths.
+# ----------------------------------------------------------
 # The path to the Skyrim / Skyrim SE installation to use
 SKYRIM_PATH="G:/steam/steamapps/common/Skyrim Special Edition"
 # The path to the vanilla Skyrim, SKSE and SkyUI sources.
@@ -19,21 +21,29 @@ SKYRIM_SOURCES="${SKYRIM_PATH}/Data/Scripts/Source"
 COMPILER="${SKYRIM_PATH}/Papyrus Compiler/PapyrusCompiler.exe"
 # The path to the flags file for Skyrim.
 FLAGS="${SKYRIM_SOURCES}/TESV_Papyrus_Flags.flg"
+
+# Variable definitions relating to the actual mod being built.
+# You will definitely want to change serveral of these.
+# ----------------------------------------------------------
 # The version to release the mod with
 VERSION="0.1.0"
 # The name of the folder in which the mod will be built
 TEMP_FOLDER="temp"
+# The name to use for the esp / bsa / modgroups / ini file.
+ESP_NAME="Anofeyn"
+# The name to use for logging and the release file
+MOD_NAME="Anofeyn"
 # The name to use for the release file
-RELEASE_NAME="Anofeyn_${VERSION}.7z"
+RELEASE_NAME="${MOD_NAME} v${VERSION}.7z"
 
 # Print a nice greeting
-echo -e "Welcome to the ${C_RED}Anofeyn release builder${C_NC}!"
+echo -e "Welcome to the ${C_GREEN}${MOD_NAME}${C_NC} release builder!"
 echo ""
 
 # Find out if we should build a dev or a release file
 release=-1
 while [[ release -eq -1 ]]; do
-    echo -e "Build a (${C_GREEN}d${C_NC})${C_YELLOW}evelopment${C_NC} or a (${C_GREEN}r${C_NC})${C_YELLOW}elease${C_NC} file?"
+    echo -e "Build a (${C_RED}d${C_NC})${C_YELLOW}evelopment${C_NC} or a (${C_RED}r${C_NC})${C_YELLOW}elease${C_NC} file?"
     read -s -n 1 key
     if [[ "${key}" == "d" ]]
     then
@@ -48,28 +58,41 @@ done
 
 # Delete and recreate a temp folder to make sure we have a fresh setup
 echo ""
-echo -e "${C_RED}==>${C_NC} Creating new temporary build folder..."
+echo -e "${C_GREEN}==>${C_NC} Creating new temporary build folder..."
 
 rm -rf "${TEMP_FOLDER}"
 mkdir -p "${TEMP_FOLDER}/Data"
 
-# Make sure MLib is here and up to date, then copy it into the temp folder
+# Copy anything we can find into the temp/Data folder
+cp -r "grass" "${TEMP_FOLDER}/Data" 2>/dev/null # TODO Can you even change / use this folder in mods?
+cp -r "lodsettings" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp -r "interface" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp -r "meshes" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp -r "music" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp -r "scripts" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp -r "seq" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp -r "sound" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp -r "strings" "${TEMP_FOLDER}/Data" 2>/dev/null # TODO Pretty sure mods can't use this folder
+cp -r "shadersfx" "${TEMP_FOLDER}/Data" 2>/dev/null # TODO Pretty sure mods can't use this folder
+cp -r "textures" "${TEMP_FOLDER}/Data" 2>/dev/null
+cp "${ESP_NAME}".* "${TEMP_FOLDER}" 2>/dev/null
+
+# Anofeyn-specific: Copy over MLib
 git submodule update --init
 cp -r "MLib/scripts" "${TEMP_FOLDER}/Data"
-
-# Copy everything else into the temp folder
-cp -r "interface" "${TEMP_FOLDER}/Data"
-cp -r "meshes" "${TEMP_FOLDER}/Data"
-cp -r "scripts" "${TEMP_FOLDER}/Data"
-cp -r "textures" "${TEMP_FOLDER}/Data"
-cp "Anofeyn.esp" "${TEMP_FOLDER}"
-cp "Anofeyn.modgroups" "${TEMP_FOLDER}"
 
 # These are only used in release mode
 if [[ $release -eq 1 ]]
 then
-    cp "AnofeynBSAManifest.txt" "${TEMP_FOLDER}"
-    cp "AnofeynBSAScript.txt" "${TEMP_FOLDER}"
+    # If they don't exist, warn the user
+    if [ ! -f "BSAManifest.txt" ] || [ ! -f "BSAScript.txt" ]
+    then
+        echo "${C_RED}Error: BSAManifest.txt and / or BSAScript.txt missing - cannot create a release file.${C_NC}"
+        exit 1
+    fi
+
+    cp "BSAManifest.txt" "${TEMP_FOLDER}"
+    cp "BSAScript.txt" "${TEMP_FOLDER}"
 fi
 
 # Move into the temp folder to make the rest of this procedure simpler
@@ -77,7 +100,7 @@ cd "${TEMP_FOLDER}"
 
 # Compile all scripts, adding the appropriate flags for each mode
 echo ""
-echo -e "${C_RED}==>${C_NC} Compiling scripts, this may take a while..."
+echo -e "${C_GREEN}==>${C_NC} Compiling scripts, this may take a while..."
 
 if [[ $release -eq 0 ]]
 then
@@ -89,22 +112,22 @@ fi
 # If we're in development mode, use loose files
 # In release mode, use a BSA
 echo ""
-echo -e "${C_RED}==>${C_NC} Packing files into an archive..."
+echo -e "${C_GREEN}==>${C_NC} Packing files into an archive..."
 if [[ $release -eq 0 ]]
 then
     cd "Data"
-    7z a "../${RELEASE_NAME}" "../Anofeyn.esp" "../Anofeyn.modgroups" "*"
+    7z a "../${RELEASE_NAME}" "../${ESP_NAME}".* "*"
 else
-    cp "${SKYRIM_PATH}/Tools/Archive/Archive.exe" "Archive.exe" # TODO This is really ugly
-    ./Archive.exe "AnofeynBSAScript.txt"
-    7z a "${RELEASE_NAME}" "Anofeyn.bsa" "Anofeyn.esp" "Anofeyn.modgroups"
+    "${SKYRIM_PATH}/Tools/Archive/Archive.exe" "BSAScript.txt"
+    rm -f "${ESP_NAME}.bsl"
+    7z a "${RELEASE_NAME}" "${ESP_NAME}".*
 fi
 
 # If we were launched in interactive mode (no parameter), wait for confirmation
 if [ -z $1 ]
 then
     echo ""
-    echo -e "${C_RED}==>${C_NC} File built as ${C_GREEN}${TEMP_FOLDER}/${RELEASE_NAME}${C_NC}!"
+    echo -e "${C_GREEN}==>${C_NC} File built as ${C_BLUE}${TEMP_FOLDER}/${RELEASE_NAME}${C_NC}!"
     echo "Press any key to continue"
     read -s -n 1
 fi
